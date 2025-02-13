6c196fb96f02c1c9c0a66a32edd38396a5fbb5a3
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-02-13 19:19:48 +0900

    [Silver V] Title: 영화감독 숌, Time: 648 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,10 +1,15 @@
 if __name__ == '__main__':
-    arr = []
-    x = 0
-    while len(arr) < 10000:
-        x += 1
-        if '666' in str(x):
-            arr.append(x)
-
     n = int(input())
-    print(arr[n-1])
+    cnt = 0
+    result = 666
+    
+    while True:
+        if '666' in str(result):
+            cnt += 1
+           
+        if cnt == n:
+            break
+        
+        result += 1
+    print(result)
+            
