7128b53092815eccd8e8a7f41ab053ee1dfca842
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-02-13 18:07:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -2,47 +2,26 @@ if __name__ == '__main__':
     n, m = map(int, input().split())
     board = []
     result = []
-    min_count1 = 64
-    min_count2 = 64
+
     for _ in range(n):
         board.append(list(input()))
 
-    for i in range(0, n - 7):
-        for j in range(0, m - 7):
-            first = 'B'
-            count = 0
+    for i in range(n - 7):
+        for j in range(m - 7):
+            draw1 = 0
+            draw2 = 0
             for row in range(i, i + 8):
                 for col in range(j, j + 8):
-                    if i % 2 == row % 2 and j % 2 == col % 2 and board[row][col] != first:
-                        count += 1
-                    if i % 2 != row % 2 and j % 2 == col % 2 and board[row][col] == first:
-                        count += 1
-                    if i % 2 == row % 2 and j % 2 != col % 2 and board[row][col] == first:
-                        count += 1
-                    if i % 2 != row % 2 and j % 2 != col % 2 and board[row][col] != first:
-                        count += 1
-
-            if count < min_count1:
-                min_count1 = count
-    result.append(min_count1)
-                
-    for i in range(0, n - 7):
-        for j in range(0, m - 7):
-            first = 'W'
-            count = 0
-            for row in range(i, i + 8):
-                for col in range(j, j + 8):
-                    if i % 2 == row % 2 and j % 2 == col % 2 and board[row][col] != first:
-                        count += 1
-                    if i % 2 != row % 2 and j % 2 == col % 2 and board[row][col] == first:
-                        count += 1
-                    if i % 2 == row % 2 and j % 2 != col % 2 and board[row][col] == first:
-                        count += 1
-                    if i % 2 != row % 2 and j % 2 != col % 2 and board[row][col] != first:
-                        count += 1
-
-            if count < min_count2:
-                min_count2 = count
-    result.append(min_count2)
-
+                    if (row + col) % 2 == 0:
+                        if board[row][col] != 'B':
+                            draw1 += 1
+                        if board[row][col] != 'W':
+                            draw2 += 1
+                    else:
+                        if board[row][col] != 'W':
+                            draw1 += 1
+                        if board[row][col] != 'B':
+                            draw2 += 1
+            result.append(draw1)
+            result.append(draw2)
     print(min(result))
