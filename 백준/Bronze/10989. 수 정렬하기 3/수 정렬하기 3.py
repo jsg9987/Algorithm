0b0b25c2daa4e6e4228894f96fd6469d09cee1fa
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-02-14 18:36:42 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 8112 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
 
     n = int(input())
     arr = [0] * 10001
-
+    
     for _ in range(n):
         arr[int(inputF().rstrip())] += 1
-
-    for i, num in enumerate(arr):
-        if num != 0:
-            for _ in range(num):
+    
+    for i in range(len(arr)):
+        if i != 0:
+            for _ in range(arr[i]):
                 print(i)
-
+            
