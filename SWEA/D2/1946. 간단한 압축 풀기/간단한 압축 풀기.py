627668582a3e00ddb2eb8550f28cc2caeac0bae2
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-03-18 22:30:02 +0900

    [D2] Title: 간단한 압축 풀기, Time: 64 ms, Memory: 51,200 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -7,18 +7,8 @@ for tc in range(1, T+1):
     for i in range(n):
         alphabet, cnt = input().split()
         cnt = int(cnt)
-        
-        while cnt !=0:
-            if len(answer) != 10:
-                empty_len = 10 - len(answer)
-                if empty_len < cnt:
-                    answer += empty_len * alphabet
-                    cnt -= empty_len
-                else:
-                    answer += cnt * alphabet
-                    cnt = 0
-            if len(answer) == 10:
-                print(answer)
-                answer = ""
-            if i == n-1 and cnt == 0:
-                print(answer)
+
+        answer += alphabet * cnt
+
+    for k in range(0, len(answer), 10):
+        print(*answer[k:k+10], sep="")
