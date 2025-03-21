1c06db5765f23e491efa0a4303cdf2122ddec2da
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-03-21 16:54:01 +0900

    [Bronze II] Title: 시각, Time: 172 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/18312. 시각/시각.py
@@ -0,0 +1,42 @@
+# n, k = map(int, input().split())
+# hour = 0
+# minute = 0
+# second = 0
+# cnt = 0
+# # 오류: 23시 59분 59초가 포함이 안되어있기 때문에 잘못 작성된 코드다.
+# while hour != n or minute != 59 or second != 59:
+#     if str(k) in str(hour) or str(k) in str(minute) or str(k) in str(second):
+#         cnt += 1
+#
+#     second += 1
+#     if second >= 60:
+#         second = 0
+#         minute += 1
+#     if minute >= 60:
+#         minute = 0
+#         hour += 1
+#
+# print(cnt)
+n, k = map(int, input().split())
+
+cnt = 0
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            str_time = ""
+            if len(str(h)) == 1:
+                str_time += '0' + str(h)
+            else:
+                str_time += str(h)
+            if len(str(m)) == 1:
+                str_time += '0' + str(m)
+            else:
+                str_time += str(m)
+            if len(str(s)) == 1:
+                str_time += '0' + str(s)
+            else:
+                str_time += str(s)
+            if str(k) in str_time:
+                cnt += 1
+
+print(cnt)
