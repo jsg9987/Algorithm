5f6a9e0b488aaccc18099618334f671579e3909f
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-03-29 15:46:04 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 244 ms, Memory: 51824 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,30 @@
+import sys
+
+inputF = sys.stdin.readline
+n, m = map(int, inputF().split())
+words = []
+
+for i in range(n):
+    word = inputF().rstrip()
+    if len(word) >= m:
+        words.append(word)
+
+# 사전 순으로 정렬
+words.sort(key= lambda x: x)
+# 단어의 길이가 길수록 앞에 배치
+words.sort(key= lambda x: len(x), reverse=True)
+dic = dict()
+
+for word in words:
+    if word in dic:
+        dic[word] += 1
+    else:
+        dic[word] = 1
+
+li = sorted(dic.items(), key= lambda x: x[1], reverse=True)
+li2 = []
+for i in li:
+    li2.append(i[0])
+
+for word in li2:
+    print(word)
