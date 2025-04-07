d1020e6fcdf8bebc1c22b5e0e1c3f03983e6f38b
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-07 21:43:30 +0900

    [Silver III] Title: N과 M (4), Time: 4760 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+s = []
+
+def dfs(start):
+    if len(s) == m:
+        check = True
+        for i in range(len(s)-1):
+            if s[i] > s[i+1]:
+                check = False
+                return
+        if check:
+            print(*s)
+        return
+    for i in range(start, n+1):
+        s.append(i)
+        dfs(start)
+        s.pop()
+
+dfs(1)
