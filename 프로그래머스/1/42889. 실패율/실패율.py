96db84d9ff8badf6d7e0e32789eec75640ea4ffb
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-14 18:45:16 +0900

    [level 1] Title: 실패율, Time: 1679.11 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,36 +1,24 @@
-# 4/14 17:28 ~
-# 실패율 = 스테이지 도달했으나 아직 클리어 x 플레이어 수 / 스테이지 도달 플레이어 수
-# 실패율이 높은 스테이지부터 "내림차순"으로 스테이지 번호가 담긴 배열 return
-# 1 <= N <= 500, 1 <= stages <= 200,000
-# N+1은 마지막 스테이지까지 클리어한 사용자
-# 실패율이 같다면 작은 번호가 먼저 오도록
-# 스테이지에 도달한 유저가 없을 경우, 해당 스테이지 실패율은 0
-
-
+# 핵심 아이디어: 해당 단계에 머물러 있는 플레이어 수를 == 도달은 했는데 클리어는 못한 사람 수
+# 머물러 있는 사람을 전체 인원에서 빼면 된다.
 def solution(N, stages):
     answer = []
-    not_clear = [0] * N
-    arrival = [0] * N
-    for stage in stages:
-        if stage != N + 1:
-            not_clear[stage - 1] += 1
-        for i in range(stage):
-            if i == N:
-                break
-            arrival[i] += 1
+    length = len(stages) # 인원 수
 
-    fail_rate = []
-    for i in range(N):
-        if arrival[i] == 0:
-            fail_rate.append((i, 0))
+    # 스테이지 1~N
+    for i in range(1, N+1):
+        # 해당 스테이지에 머물러 있는 사람의 수 계산
+        count = stages.count(i)
+
+        if length == 0:
+            fail = 0
         else:
-            fail_rate.append((i, not_clear[i] / arrival[i]))
+            fail = count / length
 
-    fail_rate.sort(key=lambda x: (-x[1], x[0]))
-    for idx, _ in fail_rate:
-        answer.append(idx + 1)
+        answer.append((i, fail))
+        length -= count
 
+    answer = sorted(answer, key=lambda x: -x[1])
+
+    answer = [i[0] for i in answer]
     return answer
 
-
-print(solution(4, [4, 4, 4, 4, 4]))
