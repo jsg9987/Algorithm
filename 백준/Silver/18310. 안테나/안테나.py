ef135bc5720bf95d14d30d28b9bc846069001fd4
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-14 16:57:32 +0900

    [Silver III] Title: 안테나, Time: 116 ms, Memory: 53288 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.py
+++ 백준/Silver/18310. 안테나/안테나.py
@@ -1,16 +1,5 @@
-import sys
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
-inputF = sys.stdin.readline
-n = int(inputF())
-li = list(map(int, inputF().rstrip().split()))
-li.sort()
-mid = len(li) // 2
-avg = sum(li) / n
-
-if mid % 2 == 0:
-    if abs(li[mid-1] - avg):
-        print(li[mid-1])
-    else:
-        print(li[mid])
-else:
-    print(li[mid])
+print(data[(n-1) // 2])
