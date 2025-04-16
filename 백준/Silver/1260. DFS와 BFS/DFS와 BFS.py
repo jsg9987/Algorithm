c052ab7949b08de9a7d0f5bc630181989c99e078
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-16 18:49:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 92 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,36 +1,32 @@
-# 4/16 17:17~
-# 조건 1. 방문할 수 있는 정점이 여러 개인 경우 작은 번호부터
-# 1 <= N <= 1000, 1 <= M <= 10000
-# 아이디어: 간선이 양방향이므로 인접 행렬을 사용
-import copy
+# 4/16 18:20~ 38
 import sys
 from collections import deque
-from copy import deepcopy
 
 INF = int(1e9)
 inputF = sys.stdin.readline
 n, m, v = map(int, inputF().split())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
-graph2 = copy.deepcopy(graph)
-for i in range(n + 1):
-    graph[i][i], graph2[i][i] = 0, 0
-for i in range(m):
+# 인접 리스트를 이용
+dfs_graph = [[] for _ in range(n+1)]
+bfs_graph = [[] for _ in range(n+1)]
+for i in range(m): # 양방향 처리 필요
     start, arrive = map(int, inputF().split())
-    graph[start][arrive], graph2[start][arrive] = 1, 1
-    graph[arrive][start], graph2[arrive][start] = 1, 1
-dfs_result = []
-bfs_result = []
+    dfs_graph[start].append(arrive)
+    bfs_graph[start].append(arrive)
+    dfs_graph[arrive].append(start)
+    bfs_graph[arrive].append(start)
 dfs_visited = [0] * (n+1)
 bfs_visited = [0] * (n+1)
 
 def dfs(v):
-    # 결과에 추가
-    dfs_result.append(v)
+    # 결과 출력
+    print(v, end=" ")
     # 방문 처리
     dfs_visited[v] = 1
-    # 다음 노드 깊이 우선 탐색
-    for i in range(n + 1):
-        if graph[v][i] == 1 and not dfs_visited[i]:
+    # 깊이 우선 탐색으로 처리
+    # for i in range(n+1): # 마찬가지로 불필요하게 모든 노드를 확인 중
+    #     if i in dfs_graph[v] and not dfs_visited[i]: 
+    for i in sorted(dfs_graph[v]):
+        if not dfs_visited[i]:
             dfs(i)
     return
 
@@ -40,19 +36,18 @@ def bfs(v):
 
     while queue:
         x = queue.popleft()
-        if bfs_visited[x]:
-            continue
-        # 결과 추가
-        bfs_result.append(x)
+        if not bfs_visited[x]:
+            # 결과 출력
+            print(x, end=" ")
         # 방문 처리
         bfs_visited[x] = 1
-        for i in range(n + 1):
-            if graph2[x][i] == 1:
+        # for i in range(n+1): # 100만, 해당 노드에서 출발해서 도착하는 경우만 보면되는데, 불필요하게 확인하고 있음.
+        #     if i in bfs_graph[x] and not bfs_visited[i]:
+        for i in sorted(bfs_graph[x]):
+            if not bfs_visited[i]:
                 queue.append(i)
     return
 
-
 dfs(v)
+print()
 bfs(v)
-print(*dfs_result)
-print(*bfs_result)
