3e349b8c998e68f0576c82fee31c1b43d94e81d4
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-24 16:23:10 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 58 ms, Memory: 52,736 KB -BaekjoonHub

---
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -0,0 +1,24 @@
+# 4/24 16:10 ~ 22
+
+for tc in range(1, 11):
+    word_len = int(input())
+    board = [input() for _ in range(8)]
+    result = 0
+
+    for row in board:
+        for j in range(8 - word_len + 1):
+            word = row[j:j + word_len]
+            word_reverse = word[::-1]
+            if word == word_reverse:
+                result += 1
+
+    for i in range(8):
+        for j in range(8 - word_len + 1):
+            temp = ""
+            for offset in range(word_len):
+                temp += board[j + offset][i]
+            temp_reverse = temp[::-1]
+            if temp == temp_reverse:
+                result += 1
+
+    print(f"#{tc} {result}")
