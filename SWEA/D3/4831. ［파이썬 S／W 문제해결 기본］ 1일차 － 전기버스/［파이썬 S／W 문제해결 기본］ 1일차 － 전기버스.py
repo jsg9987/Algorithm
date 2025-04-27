ae0499b97e99449012f70ac71a1a7e9eb030e4a0
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-27 17:47:28 +0900

    [D3] Title: [파이썬 S/W 문제해결 기본] 1일차 - 전기버스, Time: 73 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D3/4831. ［파이썬 S／W 문제해결 기본］ 1일차 － 전기버스/［파이썬 S／W 문제해결 기본］ 1일차 － 전기버스.py
+++ SWEA/D3/4831. ［파이썬 S／W 문제해결 기본］ 1일차 － 전기버스/［파이썬 S／W 문제해결 기본］ 1일차 － 전기버스.py
@@ -1,19 +1,25 @@
+from collections import deque
+
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     k, n, m = map(int, input().split())
     bus_stops = set(map(int, input().split()))
-    current = 0
-    charge = 0
+    queue = deque([(0, 0)])
+    visited = [0] * (n+1)
+    min_charge = 0
 
-    while current + k < n:
-        for move in range(k, 0, -1):
-            if current + move in bus_stops:
-                charge += 1
-                current += move
-                break
-        else:
-            charge = 0
+    while queue:
+        num, charge = queue.popleft()
+        if num + k >= n:
+            min_charge = charge
             break
 
-    print(f"#{tc} {charge}")
+        for move in range(k, 0, -1):
+            if num + move in bus_stops:
+                if not visited[num+move]:
+                    visited[num+move] = 1
+                    queue.append((num+move, charge + 1))
+
+    print(f"#{tc} {min_charge}")
+
