514777716cc8b07281af0b9b1b53bc83284104ab
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-04-29 20:30:20 +0900

    [D3] Title: 부분 수열의 합, Time: 455 ms, Memory: 73,472 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,22 +1,24 @@
-# 4/28 11:05 ~
-
-
-from itertools import combinations
 T = int(input())
 
-for tc in range(1, T+1):
+
+def dfs(idx, current_sum):
+    global result
+
+    if current_sum > k:
+        return
+    if idx == n: # 끝 인덱스에 도착했을 때 각 자리의 숫자를 골랐는지, 고르지 않았는지 확인 가능
+        if current_sum == k:
+            result += 1
+        return
+
+    dfs(idx + 1, current_sum + nums[idx])  # 현재 원소를 고르거나
+    dfs(idx + 1, current_sum)  # 고르지 않거나
+
+
+for tc in range(1, T + 1):
     n, k = map(int, input().split())
     nums = list(map(int, input().split()))
-    idxes = [i for i in range(n)]
     result = 0
 
-    for i in range(1, n+1):
-        candidates = list(combinations(idxes, i))
-        for candidate in candidates:
-            temp_sum = 0
-            for idx in candidate:
-                temp_sum += nums[idx]
-            if temp_sum == k:
-                result += 1
-
+    dfs(0,0)
     print(f"#{tc} {result}")
