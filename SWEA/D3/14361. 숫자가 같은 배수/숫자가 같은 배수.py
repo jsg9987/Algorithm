d674e768ed91a606ed16ed5dd50df0c4f7b3ba79
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-05-08 23:38:01 +0900

    [D3] Title: 숫자가 같은 배수, Time: 152 ms, Memory: 64,128 KB -BaekjoonHub

--- SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.py
+++ SWEA/D3/14361. 숫자가 같은 배수/숫자가 같은 배수.py
@@ -1,19 +1,41 @@
-# 5/8 22:30 ~ 44
-from itertools import permutations
-
+# 5/8 22:30 ~
 T = int(input())
 
-for tc in range(1, T+1):
-    n = list(input())
-    n_value = int(''.join(n))
-    length = len(n)
-    candidates = list(permutations(n, length))
 
-    for candidate in candidates:
-        if candidate[0] == '0':
-            continue
-        if int(''.join(candidate)) % n_value == 0 and int(''.join(candidate)) != n_value:
-            print(f"#{tc} possible")
-            break
+def dfs(n, result):
+    if len(result) == len(n):
+        n_value = int(''.join(n))
+        result_value = int(''.join(result))
+        if n_value != result_value and result_value % n_value == 0:
+            return True
+
+    for i in range(len(n)):
+        if len(result) == 0:
+            if n[i] == 0:
+                continue
+            else:
+                if not visited[i]:
+                    visited[i] = 1
+                    result.append(n[i])
+                    if dfs(n, result):
+                        return True
+                    result.pop()
+                    visited[i] = 0
+        else:
+            if not visited[i]:
+                visited[i] = 1
+                result.append(n[i])
+                if dfs(n, result):
+                    return True
+                result.pop()
+                visited[i] = 0
+
+
+for tc in range(1, T + 1):
+    n = list(input())
+    visited = [0] * len(n)
+    if dfs(n,[]):
+        print(f"#{tc} possible")
     else:
         print(f"#{tc} impossible")
+
