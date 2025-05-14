b41d35f61af79f831dec965f028d31785a7b7720
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-05-14 20:07:20 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -3,16 +3,4 @@ import sys
 
 inputF = sys.stdin.readline
 a, b, c = map(int, inputF().split())
-
-def solution(a,b,c):
-    if b == 1:
-        return a % c
-
-    temp = solution(a, b//2, c) # 전체의 절반 값
-
-    if b % 2 == 0:
-        return ((temp % c) * (temp % c)) % c
-    else:
-        return ((temp % c) * (temp % c) * (a % c)) % c
-
-print(solution(a,b,c))
+print(pow(a,b,mod=c))
