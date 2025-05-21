a59c98732795a29ea4d407911968f3932d67f66b
JeonSeokGyun <138769143+jsg9987@users.noreply.github.com>
2025-05-21 23:20:13 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 79 ms, Memory: 60,032 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,29 +1,60 @@
 T = int(input())
 
-for test_case in range(1, T + 1):
+
+def check_right(x, y):
+    if y + k - 1 >= n:
+        return False
+
+    for i in range(k):
+        if board[x][y + i] != 1:
+            return False
+
+    if y + k == n:
+        return True
+
+    if y + k < n:
+        if board[x][y + k] == 0:
+            return True
+
+
+def check_down(x, y):
+    if x + k - 1 >= n:
+        return False
+
+    for i in range(k):
+        if board[x + i][y] != 1:
+            return False
+
+    if x + k == n:
+        return True
+
+    if x + k < n:
+        if board[x + k][y] == 0:
+            return True
+
+
+for tc in range(1, T + 1):
     n, k = map(int, input().split())
-    puzzle = [list(map(int, input().split())) for _ in range(n)]
+    board = [list(map(int, input().split())) for _ in range(n)]
+    possible_coordinates = []
     result = 0
 
     for i in range(n):
-        word_len = 0
-
         for j in range(n):
-            # 가로
-            if puzzle[i][j] == 1:
-                word_len += 1
-            if puzzle[i][j] == 0 or j == n - 1:
-                if word_len == k:
-                    result += 1
-                word_len = 0
+            if board[i][j] == 1:
+                if i == 0:  # 위가 막힌 경우
+                    if check_down(i, j):
+                        result += 1
 
-        for j in range(n):
-            # 세로
-            if puzzle[j][i] == 1:
-                word_len += 1
-            if puzzle[j][i] == 0 or j == n - 1:
-                if word_len == k:
-                    result += 1
-                word_len = 0
-    
-    print(f"#{test_case} {result}")
+                elif board[i - 1][j] == 0:
+                    if check_down(i, j):
+                        result += 1
+
+                if j == 0:  # 왼쪽 막힌 경우
+                    if check_right(i, j):
+                        result += 1
+                elif board[i][j - 1] == 0:
+                    if check_right(i, j):
+                        result += 1
+
+    print(f"#{tc} {result}")
