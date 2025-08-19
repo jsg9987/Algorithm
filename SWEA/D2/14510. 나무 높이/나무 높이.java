6b5a698e0b27a0063f14add707e80d832756d234
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-08-19 22:27:21 +0900

    [D2] Title: 나무 높이, Time: 92 ms, Memory: 26,752 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -1,62 +1,60 @@
-// 14:53 ~ 
-// tc: 30, 2 <= N <= 100, heightMax==120
-// 최대 나무높이로 뺐을 때 1이 더 많은 경우, 2가 더 많은 경우, 같은 경우
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-
-		for (int tc = 1; tc < T + 1; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] arr = new int[N];
-			int MAX = 0;
-			int one = 0;
-			int two = 0;
-			int cnt = 0;
-			StringTokenizer st = new StringTokenizer(br.readLine());
-
-			for (int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-				MAX = Math.max(MAX, arr[i]);
-			}
-
-			for (int i = 0; i < N; i++) {
-				arr[i] = MAX - arr[i];
-				one += arr[i] % 2;
-				two += arr[i] / 2;
-			}
-
-			if (one > two) {
-				cnt += two * 2;
-				one -= two;
-				two = 0;
-				cnt += one * 2 - 1;
-			} else if (two > one) {
-				cnt += one * 2;
-				two -= one;
-				one = 0;
-				int bundle = two / 3;
-				cnt += bundle * 4;
-				two -= bundle * 3;
-				if (two == 1) {
-					cnt += two * 2;
-				} else if (two == 2) {
-					cnt += 3;
-				}
-			} else {
-				cnt += one * 2;
-			}
-
-			System.out.printf("#%d %d\n", tc, cnt);
-
-		}
-
-	}
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        
+        for(int tc = 1; tc < T+1; tc++) {
+            int N = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[N];
+            int maxValue = Integer.MIN_VALUE;
+            int one = 0;
+            int two = 0;
+            int cnt = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine().trim());
+            for(int i = 0; i < N; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+                maxValue = Math.max(maxValue, arr[i]);
+            }
+            
+            // 최대 높이와 차 구해서 업데이트하기 & one, two 개수 구하기
+            for(int i = 0; i < N; i++) {
+                arr[i] = maxValue - arr[i];    
+                one += arr[i] % 2;
+                two += arr[i] / 2;
+            }
+            
+            // one, two 비율 조정하기
+            int diff = two - one;
+            if(diff >= 3) {
+            	one += diff / 3 * 2;
+            	two -= diff / 3;
+            }
+            
+            if(one > two) {
+            	cnt += two * 2;
+            	one -= two;
+            	cnt += one * 2 -1;
+            }else if(two > one) {
+            	cnt += one * 2;
+            	two -= one;
+            	if(two == 1) {
+            		cnt += 2;
+            	}else if(two == 2) {
+            		cnt += 3;
+            	}
+            }else {
+            	cnt += one * 2;
+            }
+            
+            System.out.printf("#%d %d\n", tc, cnt);
+        }
+        
+    }
 }
