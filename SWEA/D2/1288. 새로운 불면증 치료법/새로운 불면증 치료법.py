64612d68eefd79987f9646d4137e07b3064d1603
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-08-23 21:47:42 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 58 ms, Memory: 59,520 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,26 +1,52 @@
-T = int(input())
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-for test_case in range(1, T + 1):
-    nums = set()
-    n = int(input())
-    next_n = n
-    result = 1
-
-    while True:
-        loop = 0
-        li = list(str(next_n))
-        for i in li:
-            nums.add(int(i))
-
-        next_n = (result + 1) * n
-        for i in range(10):
-            if i not in nums:
-                loop = 1
-                break
-
-        if loop:
-            result += 1
-        else:
-            break
-
-    print(f"#{test_case} {n * result}")
+public class Solution {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for(int tc = 1; tc < T+1; tc++) {
+			int N = Integer.parseInt(br.readLine().trim());
+			boolean[] checkNum = new boolean[10];
+			int checkCnt = 0;
+			int result = N;
+			int k = 2;
+			
+			String num = String.valueOf(N);
+			for(int i = 0; i < num.length(); i++) {
+				for(int j = 0; j < checkNum.length; j++) { 
+					if(!checkNum[j] && (char)(j + '0') == num.charAt(i)) { // 아직 해당 숫자가 없고 해당 숫자가 나왔을 때
+						checkCnt++;
+						checkNum[j] = true;
+					}
+				}
+			}
+			
+			if(checkCnt == 10) {
+				System.out.printf("#%d %d\n", tc, N);
+				continue;
+			}
+			
+			
+			while(checkCnt != 10) {
+				result = (k++) * N;
+				num = String.valueOf(result);
+				for(int i = 0; i < num.length(); i++) {
+					for(int j = 0; j < checkNum.length; j++) {
+						if(!checkNum[j] && (char)(j + '0')== num.charAt(i) ) {
+							checkCnt++;
+							checkNum[j] = true;
+						}
+					}
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tc, result);
+		}
+	}
+}
