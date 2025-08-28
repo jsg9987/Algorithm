a23c9e04236e96f07f2ac2b1f774723863275271
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-08-28 12:07:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 267 ms, Memory: 34,816 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -1,179 +1,122 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
- * tc: 50, 5초, 256MB, 5<=N<=100, 5<=k<=1,000, 1<=M<=1,000
- */
-
-class Node {
-	int x;
-	int y;
-	int count;
-	int dir;
-
-	public Node(int x, int y, int count, int dir) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.count = count;
-		this.dir = dir;
-	}
-
-	@Override
-	public String toString() {
-		return "Node [x=" + x + ", y=" + y + ", count=" + count + ", dir=" + dir + "]";
-	}
-}
+import org.omg.CORBA.INTERNAL;
 
 public class Solution {
-	static int N;
-	static int[][] board;
-	static Node[] nodes;
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, -1, 1 };
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//		StringBuilder sb = new StringBuilder();
-
-		int T = Integer.parseInt(br.readLine().trim());
-		for (int tc = 1; tc < T + 1; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine().trim());
-			N = Integer.parseInt(st.nextToken());
-			board = new int[N][N];
-			int M = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			nodes = new Node[K];
-
-			// board 테두리 구역 세팅
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
-						board[i][j] = -1;
-					}
-				}
-			}
-
-			// 노드 배열 초기화
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine().trim());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				int count = Integer.parseInt(st.nextToken());
-				int dir = Integer.parseInt(st.nextToken()) - 1;
-				nodes[i] = new Node(x, y, count, dir);
-			}
-
-//			System.out.println(Arrays.toString(nodes));
-
-			// M시간 동안
-			for (int m = 0; m < M; m++) {
-				// 각 노드들 이동시키기, 테두리 닿으면 값 변경하고 방향 바꾸기
-				for (int j = 0; j < K; j++) {
-					if (nodes[j] == null)
-						continue;
-					int dir = nodes[j].dir;
-					int nx = nodes[j].x + dx[dir];
-//					System.out.println("nx: "+ nx);
-					int ny = nodes[j].y + dy[dir];
-//					System.out.println("ny: " + ny);
-					int count = nodes[j].count;
-
-					if (isIn(nx, ny)) {
-						// 테두리에 닿은 경우
-						if (board[nx][ny] == -1) {
-							count /= 2;
-							if (dir == 0)
-								dir = 1;
-							else if (dir == 1)
-								dir = 0;
-							else if (dir == 2)
-								dir = 3;
-							else if (dir == 3)
-								dir = 2;
-							nodes[j].x = nx;
-							nodes[j].y = ny;
-							nodes[j].count = count;
-							nodes[j].dir = dir;
-						} else if (board[nx][ny] == 0) { // 아닌 경우
-							nodes[j].x = nx;
-							nodes[j].y = ny;
-							nodes[j].count = count;
-							nodes[j].dir = dir;
-						}
-					}
-				}
-
-//				System.out.println("이동 마치고 위치: " + Arrays.toString(nodes));
-				int maxMicrobeIdx[][][] = new int[N][N][3]; 
-				// 0: sum, 1: maxIdx, 2: maxCount
-
-				for (int i = 0; i < N; i++) {
-				    for (int j = 0; j < N; j++) {
-				        maxMicrobeIdx[i][j][1] = -1; // maxIdx 초기화
-				    }
-				}
-
-				for (int j = 0; j < K; j++) {
-				    if (nodes[j] == null) continue;
-				    Node node = nodes[j];
-				    int x = node.x, y = node.y, count = node.count;
-
-				    // 합계 누적
-				    maxMicrobeIdx[x][y][0] += count;
-
-				    // 더 큰 군집 기준으로 대표자 교체
-				    if (count > maxMicrobeIdx[x][y][2]) {
-				        maxMicrobeIdx[x][y][2] = count;
-				        maxMicrobeIdx[x][y][1] = j;
-				    }
-				}
-
-				// 최종 갱신
-				for (int j = 0; j < K; j++) {
-				    if (nodes[j] == null) continue;
-				    int x = nodes[j].x, y = nodes[j].y;
-
-				    if (maxMicrobeIdx[x][y][1] == j) {
-				        // 대표 노드 -> 합계와 dir 반영
-				        nodes[j].count = maxMicrobeIdx[x][y][0];
-				    } else {
-				        // 대표 아닌 노드 제거
-				        nodes[j] = null;
-				    }
-				}
-
-			}
-
-//				System.out.println("=== 한사이클 종료===");
-//				System.out.println("Nodes 출력: " + Arrays.toString(nodes));
-
-			// M시간이 지나고 난 후 미생물 수 return
-			int result = 0;
-			for (int i = 0; i < K; i++) {
-				if (nodes[i] != null) {
-					result += nodes[i].count;
-				}
-			}
-//			System.out.println(result);
-
-			String output = String.format("#%d %d\n", tc, result);
-			bw.write(output);
-			bw.flush();
+	
+	static class Micro {
+		int r, c, cnt, dir, total;
+		boolean isDead;
+		
+		public Micro(int r, int c, int cnt, int dir) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cnt = cnt;
+			this.dir = dir;
+			this.total = cnt;
 		}
-		bw.flush();
-		bw.close();
-		br.close();
-
 	}
-
-	static boolean isIn(int x, int y) {
-		return x >= 0 && x < N && y >= 0 && y < N;
+	static int N,M,K; 
+	static int[] dr = {0,-1,1,0,0};
+	static int[] dc = {0,0,0,-1,1};
+	static Micro[] list;
+	static Micro[][] map;
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());
+		
+		for(int tc = 1; tc < T+1; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken()); // 맵크기 5 <= N <= 100
+			M = Integer.parseInt(st.nextToken()); // 시간 1 <= M <= 1000
+			K = Integer.parseInt(st.nextToken()); // 초기 군집 수 5 <= K <= 1000
+			
+			// 전체 군집 리스트(죽은 군집(통합되어진 군집), 살아있는 군집 다 포함됨.) : 크기 변화 없음: 배열 가능
+			list = new Micro[K];
+			
+			// 매시간마다 각 셀에 이동한 미생물 정보
+			map = new Micro[N][N];
+			
+			for(int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				list[i] = new Micro(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			}
+			System.out.println("#" + tc + " " + move());
+		}
 	}
+	
+	static int move() { // 살아있는 미생물 수 리턴
+		
+		// M시간동안 군집 이동 처리
+		int time = M, nr, nc, remainCnt = 0;
+		
+		while(time-- > 0) {
+			for (Micro cur : list) {
+				if(cur.isDead) continue; // 리스트에서 삭제하지 않았으므로 건너뛰도록 처리 필요
+				
+				// 1. 한칸 이동
+				nr = cur.r += dr[cur.dir];
+				nc = cur.c += dc[cur.dir];
+				
+				// 2. 약품 칸 처리
+				if(nr == 0 || nr == N-1 || nc == 0 || nc == N-1) {
+					// 군집 줄이고 방향 바꿈, 0이면 소멸
+					cur.total = cur.cnt = cur.cnt / 2;
+					if(cur.cnt == 0) {
+						cur.isDead = true;
+						continue;
+					}
+					
+					// 소멸 안하면 방향 반대로
+					if(cur.dir % 2 == 1) cur.dir++;
+					else cur.dir--;
+				}
+				
+				// 3. 군집 병합
+				if(map[nr][nc] == null) { // 그 셀에 처음 도착하는 군집
+					map[nr][nc] = cur; 
+					
+				}else { // 나중에 도착하는 군집(다른 군집이 있는 경우)
+					// 군집의 미생물 크기로 비교해 큰쪽으로 흡수, 작은 건 소멸
+					if(map[nr][nc].cnt > cur.cnt) {
+						map[nr][nc].total += cur.cnt;
+						cur.isDead = true;
+					}else { // 나중에 도착한 군집의 크기가 크면
+						cur.total += map[nr][nc].total;
+						map[nr][nc].isDead = true;
+						map[nr][nc] = cur;
+					}
+				}
+				
+			}// end for: 군집리스트 처리
+			
+			remainCnt = reset(); // 현재 시간에 이동한 군집들 정리후 살아있는 미생물 수 받기
+		}// end while: 시간 반복
+		
+		
+		
+		return remainCnt;
+	}
+	
+	static int reset() { // 
+		int total = 0;
+		for (int r = 0; r < N; r++) {
+			for(int c = 0; c < N; c++) {
+				if(map[r][c] == null) continue;
+				// 군집이 있다면 정리
+				map[r][c].cnt = map[r][c].total;
+				total += map[r][c].cnt;
+				map[r][c] = null; // 다음 시간 처리위해 초기화
+			}
+			
+		}
+		return total;
+	}
+	
+	
 }
