82a7b213bbb0eef30069bbd209ed0846dddba089
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-09-08 11:12:54 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 96 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,65 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] costs;
-	static int[] schedules;
-	static int[] minSum;
-	static int[] durations = { 12, 3, 1, 1 };
+	static int[] cost;
+	static int[] usePlan;
+	static int[] DP;
+	static int[] periods = { 1, 1, 3, 12 };
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine().trim());
 
 		for (int tc = 1; tc < T + 1; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			costs = new int[4];
-			schedules = new int[12];
-			minSum = new int[13];
-			Arrays.fill(minSum, Integer.MAX_VALUE);
+			cost = new int[4];
+			usePlan = new int[13];
+			DP = new int[13];
+			DP[0] = 0;
+			usePlan[0] = 0;
 
-			for (int i = 3; i >= 0; i--) {
-				costs[i] = Integer.parseInt(st.nextToken());
+			StringTokenizer st = new StringTokenizer(br.readLine().trim());
+			for (int i = 0; i < 4; i++) {
+				cost[i] = Integer.parseInt(st.nextToken());
 			}
 
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 12; i++) {
-				schedules[i] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine().trim());
+			for(int i = 1; i < 13; i++) {
+				usePlan[i] = Integer.parseInt(st.nextToken());
 			}
+			
+			for (int i = 1; i < 13; i++) {
+				int localMin = Integer.MAX_VALUE;
+				for (int j = 0; j < 4; j++) {
+					if (j == 0) {
+						localMin = Math.min(localMin, DP[i - 1] + usePlan[i] * cost[j]);
+					} else {
+						localMin = Math.min(localMin,
+								(i - periods[j] >= 0) ? DP[i - periods[j]] + cost[j] : DP[0] + cost[j]);
+					}
+				}
+				DP[i] = localMin;
+			}
+			
+			System.out.printf("#%d %d\n", tc, DP[12]);
 
-			dfs(0, 0);
-
-			System.out.printf("#%d %d\n", tc, minSum[12]);
-		}
-	}
-
-	static void dfs(int month, int sum) {
-		// month가 12 넘으면 minSum 갱신
-		if (month >= 12) {
-			minSum[12] = Math.min(sum, minSum[12]);
-			return;
-		}
-		
-		// 가지치기
-		if(sum >= minSum[month]) return;
-		minSum[month] = sum;
-
-		// 4가지 요금으로 dfs + 백트래킹
-		for (int i = 0; i < 4; i++) {
-			int nextMonth = month + durations[i];
-			int nextSum = sum + nextCost(month, i);
-			dfs(nextMonth, nextSum);
-		}
-	}
-
-	static int nextCost(int month, int cost) {
-		if (cost == 3) {
-			return costs[cost] * schedules[month];
-		} else {
-			return costs[cost];
 		}
 	}
 }
