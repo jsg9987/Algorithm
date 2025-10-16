c4f1c38bdaf8d23a23c22ce2bd81dd2bb47be318
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-10-16 13:52:00 +0900

    [Gold III] Title: 파티, Time: 576 ms, Memory: 61428 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,48 +2,93 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int INF = 100_000_000;
-	public static void main(String[] args) throws IOException{
+	static int N, M, X;
+	static int INF = 1_000_000_000;
+	static List<ArrayList<Node>> map;
+	static int[][] allDist;
+
+	static class Node implements Comparable<Node> {
+		int idx;
+		int cost;
+
+		public Node(int idx, int cost) {
+			this.idx = idx;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+	}
+
+	static void dijkstra(int start) {
+		PriorityQueue<Node> pQ = new PriorityQueue<>();
+		Node first = new Node(start, 0);
+		pQ.offer(first);
+
+		int[] dist = new int[N];
+		Arrays.fill(dist, INF);
+		dist[start] = 0;
+
+		while (!pQ.isEmpty()) {
+			Node cur = pQ.poll();
+			int idx = cur.idx;
+			int cost = cur.cost;
+
+			if (dist[idx] < cost)
+				continue;
+
+			for (Node e : map.get(idx)) {
+				int to = e.idx;
+				int toCost = e.cost;
+
+				if (dist[idx] + toCost < dist[to]) {
+					dist[to] = dist[idx] + toCost;
+					pQ.offer(new Node(to, dist[idx] + toCost));
+				}
+
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			allDist[start][i] = dist[i];
+		}
+		
+		return;
+	}
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine().trim());
 
-		int N = Integer.parseInt(st.nextToken()); // 학생 수: 1 <= N <= 1_000
-		int M = Integer.parseInt(st.nextToken()); // 단방향 도로 개수: 1 <= M <= 10_000
-		int X = Integer.parseInt(st.nextToken()) - 1; // 도착지점 X
-		
-		int[][] graph = new int[N][N];
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(i == j) {
-					graph[i][j] = 0;
-				}else {
-					graph[i][j] = INF;
-				}
-			}
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken()) - 1;
+		map = new ArrayList<>();
+
+		for (int i = 0; i < N; i++) {
+			map.add(new ArrayList<>());
 		}
-		
-		for(int i = 0; i < M; i++) {
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine().trim());
-			int start = Integer.parseInt(st.nextToken())-1; // 도로 시작점
-			int end = Integer.parseInt(st.nextToken())-1; // 도로 끝점
-			int time = Integer.parseInt(st.nextToken()); // 소요시간 
-			
-			graph[start][end] = time;
+			int from = Integer.parseInt(st.nextToken()) - 1;
+			int to = Integer.parseInt(st.nextToken()) - 1;
+			int cost = Integer.parseInt(st.nextToken());
+
+			map.get(from).add(new Node(to, cost));
 		}
-		
-		for(int k = 0; k < N; k++) {
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-				}
-			}
+
+		allDist = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			dijkstra(i);
 		}
 		
 		int result = 0;
 		for(int i = 0; i < N; i++) {
-			int timeSum = graph[i][X] + graph[X][i];
-			result = Math.max(result, timeSum);
+			result = Math.max(result, allDist[i][X] + allDist[X][i]);
 		}
 		
 		bw.write(String.valueOf(result));
