8687a49faede1adc780113b329196aae79f2908b
jsg9987 <138769143+jsg9987@users.noreply.github.com>
2025-10-20 12:05:52 +0900

    [Silver I] Title: 지름길, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -3,63 +3,65 @@ import java.util.*;
 
 public class Main {
 
-	public static class Road implements Comparable<Road> {
-		int from;
-		int to;
-		int length;
+    public static class Road implements Comparable<Road> {
+        int from, to, length;
 
-		public Road(int from, int to, int length) {
-			this.from = from;
-			this.to = to;
-			this.length = length;
-		}
+        public Road(int from, int to, int length) {
+            this.from = from;
+            this.to = to;
+            this.length = length;
+        }
 
-		@Override
-		public int compareTo(Road o) {
-			return Integer.compare(this.to, o.to);
-		}
+        @Override
+        public int compareTo(Road o) {
+            return Integer.compare(this.to, o.to); // to 기준 정렬
+        }
+    }
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        int N = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
 
-		int N = Integer.parseInt(st.nextToken());
-		int D = Integer.parseInt(st.nextToken());
-		int[] DP = new int[D + 1];
-		for (int j = 0; j < D + 1; j++) {
-			DP[j] = j;
-		}
+        ArrayList<Road> roads = new ArrayList<>();
 
-		ArrayList<Road> roads = new ArrayList<>();
-		for (int i = 1; i < N + 1; i++) {
-			st = new StringTokenizer(br.readLine().trim());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int length = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int length = Integer.parseInt(st.nextToken());
 
-			if (to > D || length > to - from) {
-				continue;
-			}
+            if (to > D || length >= to - from) continue;
 
-			roads.add(new Road(from, to, length));
-		}
+            roads.add(new Road(from, to, length));
+        }
 
-		Collections.sort(roads);
-		
-		for(int i = 1; i < D+1; i++) {
-			DP[i] = Math.min(DP[i], DP[i-1] + 1);
-			
-			for(Road road : roads) {
-				if(road.to == i) {
-					int shortcut = DP[road.from] + road.length;
-					DP[i] = Math.min(DP[i], shortcut);
-				}
-			}
-		}
+        Collections.sort(roads);
 
-		System.out.println(DP[D]);
-	}
+        int[][] DP = new int[roads.size() + 1][D + 1];
+        for (int j = 0; j <= D; j++) {
+            DP[0][j] = j; // 지름길을 하나도 안 썼을 때
+        }
+
+        for (int i = 0; i < roads.size(); i++) {
+            Road r = roads.get(i);
+            int from = r.from;
+            int to = r.to;
+            int len = r.length;
+
+            for (int j = 0; j <= D; j++) {
+                // 기본적으로 이전 값 복사
+                DP[i + 1][j] = DP[i][j];
+
+                // 지름길을 통해 갱신할 수 있는 거리 j만 갱신
+                if (j >= to) {
+                    DP[i + 1][j] = Math.min(DP[i + 1][j], DP[i][from] + len + (j - to));
+                }
+            }
+        }
+
+        System.out.println(DP[roads.size()][D]);
+    }
 }
